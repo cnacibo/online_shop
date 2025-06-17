c66c7547f958de5ac57574607945cc906982dd44
Vlasova Mariia <vlasovam014@gmail.com>
2025-06-17 14:49:31 +0300

    add enum check in payment_result_consumer.py

--- OrdersService/app/infrastructure/payment_result_consumer.py
+++ OrdersService/app/infrastructure/payment_result_consumer.py
@@ -4,6 +4,9 @@ import json
 from app.infrastructure.database import AsyncSessionLocal
 from app.infrastructure.repositories import OrderModel
 from sqlalchemy import select
+from app.domain.enums import OrderStatus
+import logging
+logger = logging.getLogger(__name__)
 
 async def start_payment_result_consumer():
     connection = await aio_pika.connect_robust("amqp://guest:guest@rabbitmq/")
@@ -14,7 +17,14 @@ async def start_payment_result_consumer():
         async with message.process():
             data = json.loads(message.body)
             order_id = data.get("order_id")
-            status = data.get("status")
+            status_str = data.get("status")
+
+            try:
+                status = OrderStatus(status_str)
+            except ValueError as e:
+                logger.warning(f"Unknown order status received: {status_str} for order_id={order_id} | ERROR: {e}")
+                return
+
 
             async with AsyncSessionLocal() as session:
                 result = await session.execute(select(OrderModel).where(OrderModel.id == order_id))
@@ -22,6 +32,8 @@ async def start_payment_result_consumer():
                 if order:
                     order.status = status
                     await session.commit()
+                else:
+                    logger.warning(f"Order not found: order_id={order_id}")
 
     await queue.consume(handle_message)
 
